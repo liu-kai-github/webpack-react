df93348f390e3d893a60642ad6c63789d870fbbb
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2017-12-13 14:46:00 +0800

    进一步优化项目

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import App from './app/App';
-
-// class App extends React.Component {
-//     render() {
-//         return (
-//             <div className="App">111
-//                 {/*<header className="App-header">*/}
-//                 {/*<img src={logo} className="App-logo" alt="logo"/>*/}
-//                 {/*<h1 className="App-title">Welcome to React</h1>*/}
-//                 {/*</header>*/}
-//                 {/*<p className="App-intro">*/}
-//                 {/*To get started, edit <code>src/App.js</code> and save to reload.*/}
-//                 {/*</p>*/}
-//                 {/*<DatePicker/>*/}
-//             </div>
-//         );
-//     }
-// }
+import App from './app';
 
 ReactDOM.render(
     <App />,
