3116c7b545deea43a64999ad6dd5e8ac3bce3a1c
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2017-12-13 14:41:40 +0800

    添加react内容，进一步进行测试

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-console.log('333');
+import App from './app/App';
+
+// class App extends React.Component {
+//     render() {
+//         return (
+//             <div className="App">111
+//                 {/*<header className="App-header">*/}
+//                 {/*<img src={logo} className="App-logo" alt="logo"/>*/}
+//                 {/*<h1 className="App-title">Welcome to React</h1>*/}
+//                 {/*</header>*/}
+//                 {/*<p className="App-intro">*/}
+//                 {/*To get started, edit <code>src/App.js</code> and save to reload.*/}
+//                 {/*</p>*/}
+//                 {/*<DatePicker/>*/}
+//             </div>
+//         );
+//     }
+// }
 
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <App />,
     document.getElementById('root')
 );
