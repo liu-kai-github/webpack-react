bd7d3cbfe9cbfe4153f592f95984dd77345ec6dc
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2017-12-14 11:43:31 +0800

    完善react eslint

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -3,11 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
+                    <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
                 <p className="App-intro">
